aa0be6e8e9706c08511a8f71e1e2dd34440f5f74
Akila Udara <142371848+it23372726@users.noreply.github.com>
2026-08-10 17:17:21 +0530

    test success

--- hrms-frontend/src/pages/LoginPage.jsx
+++ hrms-frontend/src/pages/LoginPage.jsx
@@ -149,7 +149,7 @@ export default function LoginPage() {
                             {applicationName}
                         </Typography>
                         <Typography variant="caption" sx={{ color: "rgba(255,255,255,0.68)" }}>
-                            Human Resources Managementttttttttttttt
+                            Human Resources Management
                         </Typography>
                     </Box>
                 </Stack>
